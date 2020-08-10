b3130956601c6b3f810a79fd2c6ee783f323545a
Meron <49148453+Meronada@users.noreply.github.com>
2020-08-10 16:13:09 +0200

    'postresql'

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
+LOGIN_REDIRECT_URL = '/'
 
 # Application definition
 
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER': 'meron',
+        'PASSWORD': 'meron',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
